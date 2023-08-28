725424d3fb205046621756438e0ec5ff49ee173d
Stepjet <stepjet@mail.ru>
2023-08-28 16:19:34 +0300

    workflow

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gologin',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Deps
+    ],
+    author='GoLogin',
+    author_email='yuri@gologin.com',
+    description='Official GoLogin python package',
+    url='https://github.com/gologinapp/pygologin',
+)
