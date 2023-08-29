060d423c08ab8cd21c24c2be03ccdcc1a8bd7153
Stepjet <stepjet@mail.ru>
2023-08-29 12:32:54 +0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gologin',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         # Deps
