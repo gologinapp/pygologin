7d895c6798643bc180a133ed364c4a79c722f15b
igortikushEasync <igor@easync.io>
2025-04-22 14:51:58 +0200

    fixes

--- gologin/gologin-selenium_4_11.py
+++ gologin/gologin-selenium_4_11.py
@@ -3,6 +3,7 @@ from gologin import GoLogin
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
 profile_id = "Your profile id"
 
 # Initialize GoLogin
@@ -11,14 +12,15 @@ gl = GoLogin({
 	"profile_id": profile_id
 	})
 
+# Add proxy to the profile
+gl.addGologinProxyToProfile(profile_id, "us")
+
 # Start Browser and get websocket url
 debugger_address = gl.start()
 
 # Get Chromium version for webdriver
 chromium_version = gl.get_chromium_version()
 
-# Add proxy to profile
-gl.addGologinProxyToProfile(profile_id, "us")
 
 # Install webdriver
 service = Service(ChromeDriverManager(driver_version=chromium_version).install())
@@ -27,11 +29,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("debuggerAddress", debugger_address)
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
-gl = GoLogin({
-	"token": "your token",
-	})
-profile = gl.createProfileRandomFingerprint({"os": "lin", "name": "some name"})
-gl.setProfileId(profile['id'])
+
 # Give command to Selenium to open the page
 driver.get("http://www.python.org")
 
