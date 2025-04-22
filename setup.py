1a570609e1b81d25b98bb02787b2c5e47681be60
igortikushEasync <igor@easync.io>
2025-04-22 17:27:40 +0200

    Bump version: 2025.04.22165958 → 2025.04.22172740

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='gologin',
-    version='2025.04.22165958',
+    version='2025.04.22172740',
     packages=find_packages(),
     install_requires=install_requires,
     author='GoLogin',
